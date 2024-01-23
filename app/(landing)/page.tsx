40246f2edb0bf74b5ac9ef02cf1cf4123a3123a3
Mehul Goel <111099204+mehulgo93@users.noreply.github.com>
2024-01-22 20:13:50 -0500

    finishing some code

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,20 +1,13 @@
+import { LandingHero } from "@/components/landing-hero";
+import { LandingNavbar } from "@/components/landing-navbar";
 import React from "react";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
 
 const LandingPage = () => {
   return (
-    <div>
-      {" "}
-      LandingPage(Unprotected)
-      <div>
-        <Link href="/sign-in">
-          <Button>Login</Button>
-        </Link>
-        <Link href="/sign-up">
-          <Button>Register</Button>
-        </Link>
-      </div>
+    <div className="h-full">
+      <LandingNavbar />
+      <LandingHero />
+      <LandingContent />
     </div>
   );
 };
